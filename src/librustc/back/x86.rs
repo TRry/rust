e7a0561f79927b56ffff17a72f7c5599b25122e8
Valerii Hiora <valerii.hiora@gmail.com>
2014-05-29 08:54:43 +0300

    Updated to compile with latest nightly

--- src/librustc/back/x86.rs
+++ src/librustc/back/x86.rs
@@ -36,7 +36,7 @@ pub fn get_target_strs(target_triple: String, target_os: abi::Os)
                 -i32:32:32-i64:32:64\
                 -f32:32:32-f64:32:64-v64:64:64\
                 -v128:128:128-a0:0:64-f80:128:128\
-                -n8:16:32".to_strbuf()
+                -n8:16:32".to_string()
           }
 
           abi::OsWin32 => {
